89150ed662fdb81f53363b71e95a3bf116615c56
renshou753@gmail.com <ubuntu@ip-172-31-69-60.ec2.internal>
2020-06-26 11:00:53 +0000

    update

---
+++ src/lambda/auth/auth0Authorizer.ts
@@ -0,0 +1,61 @@
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import 'source-map-support/register'
+
+
+
+export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
+  try {
+    verifyToken(event.authorizationToken)
+    console.log('User was authorized')
+
+    return {
+      principalId: 'user',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  } catch (e) {
+    console.log('User was not authorized', e.message)
+
+    return {
+      principalId: 'user',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Deny',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  }
+}
+
+function verifyToken(authHeader: string){
+  if (!authHeader)
+    throw new Error('No authentication header')
+
+  if (!authHeader.toLowerCase().startsWith('bearer '))
+    throw new Error('Invalid authentication header')
+
+  const split = authHeader.split(' ')
+  const token = split[1]
+  
+  if (token != '123'){
+      throw new Error('invalid token')
+  }
+  
+
+  // request has been authrorized
+}
+
+
